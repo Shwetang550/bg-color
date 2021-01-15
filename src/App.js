1cd832e2750bb64d2cb4481d7f6e5fe3b6fab5c3
Shwetang <shwetangsingh5@gmail.com>
2021-01-15 19:38:55 +0530

    bootstrap card added

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ const App = () => {
    };
 
   return ( 
-    <>
-      <div style={{ backgroundColor: bgColor }}>
-        <input onChange={handleChange} type="text" />
+    <div className="mx-auto" style={{ backgroundColor: bgColor, width: "100%" }}>
+      <div className="card" style={{ width: "23rem" }}>
+        <div className="card-body">
+          <h2 className="card-title">{bgColor}</h2>
+          <p className="card-text">Change the background color here :)</p>
+          <input onChange={handleChange} type="text" />
+        </div>
       </div>
-    </>
+      </div>
    );
 }
  
