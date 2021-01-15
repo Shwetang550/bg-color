f63eca7e36de9ac87f20315725e3158905d1acbd
Shwetang <shwetangsingh5@gmail.com>
2021-01-15 22:10:34 +0530

    card centered

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './app.css';
 
 const App = () => {
   let [bgColor, setBgColor] = useState("white");
@@ -9,15 +10,18 @@ const App = () => {
    };
 
   return ( 
-    <div className="mx-auto" style={{ backgroundColor: bgColor, width: "100%" }}>
-      <div className="card" style={{ width: "23rem" }}>
-        <div className="card-body">
-          <h2 className="card-title">{bgColor}</h2>
-          <p className="card-text">Change the background color here :)</p>
-          <input onChange={handleChange} type="text" />
-        </div>
-      </div>
-      </div>
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column" id="myDiv" style={{ backgroundColor: bgColor }}>
+      <main className="px-3">
+        <div className="card" style={{ width: "23rem", margin: "17rem auto" }}>
+         <div className="card-body">
+           <h2 className="card-title">{bgColor}</h2>
+            <p className="card-text" style={{marginBottom: "0"}}>Change the background color here :)</p>
+            <p className="card-text">(color-name, rgba-code, anything)</p>
+           <input onChange={handleChange} type="text" />
+         </div>
+       </div>
+      </main>
+    </div>
    );
 }
  
