aed95c3d91d944f2cd4c28e0241ae4fa0475c141
Shwetang <shwetangsingh5@gmail.com>
2021-01-17 17:47:10 +0530

    form event handler update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import './app.css';
 
 const App = () => {
   let [bgColor, setBgColor] = useState("");
+  let [currentBgColor, setCurrentBgColor] = useState("");
 
   const handleChange = (i) => {
     bgColor = i.target.value;
     setBgColor(bgColor);
-   };
+  };
+
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    setCurrentBgColor(bgColor);
+  };
 
   return ( 
-    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column" id="myDiv" style={{ backgroundColor: bgColor }}>
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column" id="myDiv" style={{ backgroundColor: currentBgColor }}>
       <main className="px-3">
         <div className="card" style={{ width: "23rem", margin: "17rem auto" }}>
          <div className="card-body">
-           <h2 className="card-title">{bgColor}</h2>
+           <h2 className="card-title">{currentBgColor}</h2>
             <p className="card-text" style={{ marginBottom: "0" }}>Change the background color here :)</p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <input placeholder="Enter any color" onChange={handleChange} type="text" value={bgColor} />
               <button className="btn btn-primary btn-sm" type="submit" style={{marginTop: "0.5rem", display: "block"}}>Submit</button>
             </form>
