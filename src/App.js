9de8fe3ae0ea69ea97b762d2cad73697be6a1d7d
Shwetang <shwetangsingh5@gmail.com>
2021-01-17 17:30:20 +0530

    button added to form

--- src/App.js
+++ src/App.js
@@ -15,8 +15,11 @@ const App = () => {
         <div className="card" style={{ width: "23rem", margin: "17rem auto" }}>
          <div className="card-body">
            <h2 className="card-title">{bgColor}</h2>
-            <p className="card-text" style={{marginBottom: "0"}}>Change the background color here :)</p>
-            <input placeholder="Enter any color" onChange={handleChange} type="text" value={bgColor} />
+            <p className="card-text" style={{ marginBottom: "0" }}>Change the background color here :)</p>
+            <form>
+              <input placeholder="Enter any color" onChange={handleChange} type="text" value={bgColor} />
+              <button>Submit</button>
+            </form>
          </div>
        </div>
       </main>
